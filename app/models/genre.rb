944b539ead02b44788517c06b896b67f8582227a
MZYugu <manami.u.yugu@gmail.com>
2023-08-12 04:06:01 +0900

    genreモデルの作成

---
+++ app/models/genre.rb
@@ -0,0 +1,17 @@
+class Genre < ApplicationRecord
+  self.data = [
+    { id: 1, name: '---'},
+    { id: 2, name: '演劇'},
+    { id: 3, name: '古典芸能'},
+    { id: 4, name: 'スポーツ観戦'},
+    { id: 5, name: 'トークショー'},
+    { id: 6, name: '握手会・グリーティング'},
+    { id: 7, name: 'ライブ・コンサート・フェス'},
+    { id: 8, name: '即売会'},
+    { id: 9, name: '展示会'},
+    { id: 10, name: 'その他'}
+  ]
+
+  include ActiveHash::Associations
+  has_many :events
+end
