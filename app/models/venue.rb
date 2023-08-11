f3ae4d34b7e6973458e5f79d415ca26117243719
MZYugu <manami.u.yugu@gmail.com>
2023-08-11 21:59:28 +0900

    バリデーションの修正

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -6,7 +6,6 @@ class Venue < ApplicationRecord
     validates :venue_name
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank."}
     validates :address
-    validates :link
   end
   
 end
