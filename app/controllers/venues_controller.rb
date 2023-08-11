2285809aa9f005738bdb1744d9fa073b219a49d5
MZYugu <manami.u.yugu@gmail.com>
2023-08-11 22:15:05 +0900

    venuesコントローラーの記述

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,9 +1,22 @@
 class VenuesController < ApplicationController
-  before_action :[:new, :edit]
+  before_action :get_venue: [:show, :edit, :update]
+  before_action :authenticate_user! except: [:index, :show]
+
   def index
+    @venues - Venue.all
   end
 
   def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    if @venue.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -11,11 +24,21 @@ class VenuesController < ApplicationController
 
   def edit
   end
+  
+  def update
+    if @venue.update(venue_params)
+      redirect_to venue_path
+    else
+      render :edit
+    end
+  end
 
   privete
   def venue_params
+    params.require(:venue).permit(:venue_name, :prefecture_id, :address, :link)
   end
 
   def get_venue
+    @venue = Venue.find(params[:id])
   end
 end
