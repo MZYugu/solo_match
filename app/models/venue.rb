0962b1ae5db229ff3d68e9c1203b1815cd85e2eb
MZYugu <manami.u.yugu@gmail.com>
2023-08-11 21:45:32 +0900

    バリデーションの設定

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,12 @@
 class Venue < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+  
+  with_options presence: true do
+    validates :venue_name
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank."}
+    validates :address
+    validates :link
+  end
+  
 end
