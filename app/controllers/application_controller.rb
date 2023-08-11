e17e840650891fc34926c6d6f1fae79e74e835dc
MZYugu <manami.u.yugu@gmail.com>
2023-08-11 20:28:30 +0900

    ストロングパラメータの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_paramerters, if: devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-  def configure_permitted_paramerters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :introduction, :birthday])
   end
 end
