9e86e0aa1b6f7b9e37adc2f01d8a82a3cd4a8003
MZYugu <manami.u.yugu@gmail.com>
2023-08-11 21:37:55 +0900

    アソシエーションの設定

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,8 +1,4 @@
 class Venue < ApplicationRecord
-<<<<<<< Updated upstream
-end
-=======
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
 end
->>>>>>> Stashed changes
