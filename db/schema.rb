12bffb43e7dc012260af31e9a24f678155a67692
MZYugu <manami.u.yugu@gmail.com>
2023-08-11 21:29:29 +0900

    venuesデータベースの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_11_102343) do
+ActiveRecord::Schema.define(version: 2023_08_11_112945) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "user_name", null: false
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2023_08_11_102343) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "venues", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "venue_name", null: false
+    t.integer "prefecture_id", null: false
+    t.string "address", null: false
+    t.string "link", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
