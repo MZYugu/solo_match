7532cd05f3c99dfd9b35cbe0be456867f01877be
MZYugu <manami.u.yugu@gmail.com>
2023-08-12 01:57:12 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "events#index"
-  resources :venues, only: [:index, :create, :show, :edit]
+  resources :venues, only: [:index, :new, :create, :show, :edit]
 end
