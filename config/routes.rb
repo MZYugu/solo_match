f0737ea7c151fba8797f226365b12b2ef4f94176
MZYugu <manami.u.yugu@gmail.com>
2023-08-12 03:30:44 +0900

    トップページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "events#index"
-  resources :venues, only: [:index, :new, :create, :show, :edit]
+  root to: "pages#home"
+  resources :venues, only: [:index, :new, :create, :show, :edit] do
+    resources :events,only: [:index, :new, :edit]
+  end
 end
