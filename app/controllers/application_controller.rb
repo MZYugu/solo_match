51bf9d8682b5eeb17a2a0d40e6f31b55e3b66a2c
MZYugu <manami.u.yugu@gmail.com>
2023-08-11 18:05:22 +0900

    ユーザー登録画面の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramerters, if: devise_controller?
+
+  private
+  def configure_permitted_paramerters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :birthday])
+  end
 end
