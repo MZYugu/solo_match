476cde1233910e5846830199cbf0cb27337d53f5
MZYugu <manami.u.yugu@gmail.com>
2023-08-12 12:17:31 +0900

    topページの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :venues, only: [:index, :new, :create, :show, :edit]
-  resources :events, only: [:index, :new, :create, :show, :edit]
+  resources :venues, only: [:index, :new, :create, :show, :edit] do
+    resources :events,only: [:index, :new, :create, :show, :edit]
+  end
 end
